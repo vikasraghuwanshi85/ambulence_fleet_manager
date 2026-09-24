88b58755c2a4d74a6574c690f8259d7975114c41
Vikas Raghuwwanshi <vikas@dotcompliancegroup.com>
2026-09-24 15:33:29 +0530

    Added bill log and image uploads

--- sw.js
+++ sw.js
@@ -1,22 +1,18 @@
 
-const CACHE_NAME = 'ambulance-fleet-v1';
-const urlsToCache = ['./','./index.html','./manifest.json'];
+const CACHE_NAME = 'janani-fleet-v73';
+const urlsToCache = ['index.html', 'manifest.json'];
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(self.clients.claim());
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(response => {
-      return response || fetch(event.request);
+      if (response) return response;
+      return fetch(event.request);
     })
   );
 });
